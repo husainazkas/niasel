cc12578e1fe4919ff43abd9356a458b94727d320
Husain Fadhilah <husainazkas@gmail.com>
2024-04-29 06:48:22 +0800

    feat: Integrate table and form to database feat: Add form validators and user interaction

---
+++ src/pos/controller/ProductController.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pos.controller;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import pos.model.Product;
+import pos.model.User;
+
+/**
+ *
+ * @author husainazkas
+ */
+public class ProductController extends BaseController {
+
+    private List<Product> products = new ArrayList();
+
+    /**
+     *
+     * @param tableModel must be provided from
+     * {@code javax.swing.JTable.getModel()}
+     */
+    public void loadProducts(TableModel tableModel) {
+        try (final EntityManager em = emf.createEntityManager()) {
+            TypedQuery<Product> query = em.createQuery("SELECT p FROM Product p WHERE p.isDeleted = false", Product.class);
+            products = query.getResultList();
+        } catch (Exception ex) {
+            Logger.getLogger(ProductController.class.getName()).log(Level.SEVERE, ex.getMessage());
+        }
+
+        DefaultTableModel table = (DefaultTableModel) tableModel;
+        table.setRowCount(0);
+        for (int i = 0; i < products.size(); i++) {
+            Product p = products.get(i);
+            Object[] row = {Long.valueOf(String.valueOf(i + 1)), p.getId(), p.getBarcodeId(), p.getName(), p.getPrice(), p.getStock(), p.getBrand()};
+            table.addRow(row);
+        }
+    }
+
+    /**
+     *
+     * @param index must be provided from
+     * {@code javax.swing.JTable.getSelectedRow()}, this may be null
+     * @return the {@code product}, if present, otherwise {@code null}
+     */
+    public Product getSelectedProduct(int index) {
+        if (index == -1) {
+            return null;
+        }
+
+        try {
+            return products.get(index);
+        } catch (IndexOutOfBoundsException ex) {
+            return null;
+        }
+    }
+
+    public void save(User user, int index, String barcodeId, String name, String price, String stock, String brand) {
+        try (final EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+
+            Product product = getSelectedProduct(index);
+            if (product != null) {
+                product.setBarcodeId(barcodeId);
+                product.setName(name);
+                product.setPrice(Long.valueOf(price));
+                product.setStock(Integer.valueOf(stock));
+                product.setBrand(brand);
+                product.setUpdatedBy(user.getId());
+                em.merge(product);
+            } else {
+                product = new Product();
+                product.setBarcodeId(barcodeId);
+                product.setName(name);
+                product.setPrice(Long.valueOf(price));
+                product.setStock(Integer.valueOf(stock));
+                product.setBrand(brand);
+                product.setUpdatedBy(user.getId());
+                product.setCreatedBy(user.getId());
+                em.persist(product);
+            }
+
+            em.getTransaction().commit();
+        }
+    }
+
+    public void remove(User user, int index) {
+        Product product = getSelectedProduct(index);
+        if (product == null) {
+            return;
+        }
+        try (final EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+
+            product.setIsDeleted(true);
+            product.setUpdatedBy(user.getId());
+            em.merge(product);
+
+            em.getTransaction().commit();
+        }
+    }
+}
